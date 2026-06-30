21f72e756cad6dd849e9003286cd89b67be27a1d
Heth Shah <145697051+Hethshah12@users.noreply.github.com>
2026-06-30 11:29:10 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # if len(s)!=len(t):
+        #     return False
+        # s=sorted(s)
+        # t=sorted(t)
+        # #now the strings are immutable so we cannot use s.sort(), always use sorted(s)
+        # #they return a list 
+        # for i in range(len(s)):
+        #     if s[i]!=t[i]:
+        #         return False
+        # return True
+
+        #faster method with o(n) complexity
+        if len(s)!=len(t):
+            return False 
+        s_h=dict()
+        t_h=dict()
+        for ch in s:
+            s_h[ch] = s_h.get(ch,0)+1
+
+        for ch in t:
+            t_h[ch] = t_h.get(ch,0)+1
+
+        return s_h==t_h
+        
+        
