300e11511a13cbd65b46bc2413bbd3ba04265066
Heth Shah <145697051+Hethshah12@users.noreply.github.com>
2026-07-27 15:38:36 +0530

    Add: house-robber - submission-15

---
+++ Data Structures & Algorithms/house-robber/submission-15.py
@@ -0,0 +1,50 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        #bottom up tabulation 
+        n=len(nums)
+        dp=[0]*(n+1)
+        
+        if n==1:
+            return nums[0]
+        if n==2:
+            return max(nums[0], nums[1])
+        dp[0]=nums[0]
+        dp[1]=max(nums[0], nums[1])
+
+        for i in range(2, n):
+            dp[i]=max(dp[i-1], nums[i]+dp[i-2], nums[i])
+        return dp[n-1]
+
+#Top Down DP Memoization 
+        # n=len(nums)
+        
+        # if n==1:
+        #         return nums[0]
+        # if n==2:
+        #     return max(nums[0], nums[1])
+        # memo={0:nums[0], 1:max(nums[0], nums[1])}
+        # def helper(i): 
+        #     if i in memo:
+        #         return memo[i]
+        #     else:
+        #         memo[i]=max(helper(i-1), nums[i]+helper(i-2))
+        #     return memo[i]
+        
+        # return helper(n-1)
+
+
+
+
+        #recursive solution
+        # n=len(nums)
+
+        # def helper(i):
+        #     if i==0:
+        #         return nums[i]
+            
+        #     if i==1:
+        #         return max(nums[0], nums[1])
+            
+        #     return max(helper(i-1),nums[i]+helper(i-2))
+        
+        # return helper(n-1)
